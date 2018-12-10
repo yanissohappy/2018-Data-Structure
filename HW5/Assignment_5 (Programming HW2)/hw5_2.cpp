1a1849b2dbf15cfa21632654be39ce8f8fad98e6
Yan <t19960720@gmail.com>
2018-12-10 12:58:49 +0800

    Hw5

---
+++ HW5/Assignment_5 (Programming HW2)/hw5_2.cpp
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <string.h>
+
+void search_delete_array(int arr[100],int modified);
+void swap(int &p1, int &p2);
+void MaxHeapify(int array[100], int root, int length);
+int HowManyInArray(int arr[100]);
+void print_arr(int arr[100],int count);
+
+
+int main()
+{
+	
+	char str[10000]={};
+	int arr[100]={0};
+	int t,i=0;
+	int index = 0;
+	int be_modified;
+	
+	for(int i=0;i<100;i++)
+	{
+		arr[i]=-9999999;
+	}		
+	
+	gets(str);//get first line	
+	char *p = strtok(str, " ");	
+	
+	while (p != NULL && index < 100) {
+	  arr[index++] = atoi(p);
+	  p = strtok(NULL, " ");
+	}
+	
+	gets(str);//get secone line	
+	be_modified=atoi(str);
+
+	search_delete_array(arr,be_modified);	
+	print_arr(arr,HowManyInArray(arr));	
+
+    return 0;
+}
+
+void search_delete_array(int arr[100],int modified)
+{
+	int i=0;
+	for(i=0;i<100;i++)
+	{
+		if(arr[i]==modified){
+			arr[i]=arr[HowManyInArray(arr)-1];	
+			arr[HowManyInArray(arr)-1]=-9999999;
+			MaxHeapify(arr, i, HowManyInArray(arr));
+			break;
+		}
+	}
+	
+	MaxHeapify(arr,i, HowManyInArray(arr));
+	
+	while(arr[i]>arr[(i-1)/2] && i>=0)
+	{
+		MaxHeapify(arr,(i-1)/2, HowManyInArray(arr));
+		i=(i-1)/2;
+	}	
+	
+}
+
+int HowManyInArray(int arr[100])
+{
+	int count=0;
+	for(int i=0;i<100;i++)
+	{
+		if(arr[i]!=-9999999)
+		count++;
+			
+		if(arr[i]<=-9999999)
+		break;
+	}
+	return count;	
+}
+
+void swap(int &p1, int &p2)
+{
+    int temp = p1;
+    p1 = p2;
+    p2 = temp;
+}
+
+void MaxHeapify(int array[100], int root, int length)
+{
+
+    int left = 2*root+1;      
+    int right = 2*root + 2;     
+    int largest;                
+
+    if (left <= length && array[left] > array[root])
+        largest = left;
+    else
+        largest = root;
+
+    if (right <= length && array[right] > array[largest])
+        largest = right;
+
+    if (largest != root) {                         
+        swap(array[largest], array[root]);         
+        MaxHeapify(array, largest, length);        
+    }
+}
+
+void print_arr(int arr[100],int count)
+{
+	for(int i=0;i<count;i++)
+	{
+		printf("%d ",arr[i]);
+	}
+}
+
